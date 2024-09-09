1325e539fe73a5b30112fd9067c3057271b46431
co-en <reinhardtco395@northwested.org>
2024-09-09 10:30:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,18 @@
 
 # Exercise 2 - 3  (Personal Message)
 
-
+person_name = "Nathan"
+Print"[person_name] do you want to play tennis?"
 
 #  Exercise 2 - 5 (Famous Quote)
 
+famous_name = 'Vince Lombardi'
+famous_quote = "Winning isn't everything, but wanting to win is."
+Print"[famous_name] said, [famous_quote]
 
 
 # Exercise 2 - 6 (Famous Quote 2)
 
-
+famous_name = 'Vince Lombardi'
+message = "Winning isn't everything, but wanting to win is."
+Print"[famous_name] said, [message]
